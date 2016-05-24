aecf281ffd45d0dd6784a9229370bc75de982acd
vzayats <vzayats3@gmail.com>
2016-05-24 13:13:33 +0300

    Add NetworkInfo class

---
+++ ExampleApp/NetworkInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ExampleApp
+{
+    class NetworkInfo
+    {
+        public void GetIPAddress()
+        {
+            //Information about host name of the local computer
+            string hostName = Dns.GetHostName();
+            Console.WriteLine("Host name: {0}", hostName);
+
+            //Get the IP
+            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            Console.WriteLine("My IP Address is: {0}", myIP);
+            Console.ReadKey();
+        }
+    }
+}
